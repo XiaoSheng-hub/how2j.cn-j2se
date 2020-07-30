f0e04c682ce9680d49511e0ffebc4b3d4b9bfbf4
XiaoSheng <wang_xiao_sheng@126.com>
2020-07-31 01:00:59 +0800

    两份文件

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("hello hello");
-        System.out.println("hello hello hello");
-        System.out.println("hello hello hello hello");
-    }
-}
